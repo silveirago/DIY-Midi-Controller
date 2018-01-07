440f64766caf0b61208237f40689c17440392054
Gustavo Silveira <silveira.go@gmail.com>
2018-01-07 14:04:28 -0500

    Arduino (pro) Micro & Leonardo suport added

--- Code - código/en-DIY_midi_controller/en-DIY_midi_controller.ino
+++ Code - código/en-DIY_midi_controller/en-DIY_midi_controller.ino
@@ -1,116 +1,143 @@
-/* 
-Made by Gustavo Silveira, 2016.
-This Sketch reads the Arduino's digital and analog ports and send midi notes and midi control change
+/*
+  Made by Gustavo Silveira, 2016.
+  This Sketch reads the Arduino's digital and analog ports and send midi notes and midi control change
 
-http://www.musiconerd.com
-http://www.bitcontrollers.com
+  http://www.musiconerd.com
+  http://www.bitcontrollers.com
 
-If you are using for anything that's not personal use don't forget to give credit.
+  If you are using for anything that's not personal use don't forget to give credit.
 
-gustavosilveira@musiconerd.com
+  gustavosilveira@musiconerd.com
 */
 
-#include <MIDI.h>
-MIDI_CREATE_DEFAULT_INSTANCE();
+
+// Uncomment if using with ATmega328 - Uno, Mega, Nano...
+//#include <MIDI.h>
+//MIDI_CREATE_DEFAULT_INSTANCE();
+
+// Uncomment if using with ATmega32U4 - Micro, Pro Micro, Leonardo...
+#include "MIDIUSB.h"
 
 /////////////////////////////////////////////
-
-const int NButtons = 4; // *Enter here the number of digital inputs used 
-const int button[NButtons] = {2, 3, 4, 5}; // *In this array enter the pins of the digital ports used
-int buttonCState[NButtons] = {0}; // Current state of the digital port
-int buttonPState[NButtons] = {0}; // Previous state of the digital port
-
-/////////////////////////////////////////////
-
-const int NPots = 1; // *Enter here the number of analog inputs used
-const int pot[NPots] = {A0}; // *In this array enter the pins of the analog ports used in the desired order
-int potCState[NPots] = {0}; // Current state of the analog port
-int potPState[NPots] = {0}; // Previous state of the analogue port
-int potVar = 0; // Variation between the value of the previous state and the current value of the analogue port
+// buttons
+const int NButtons = 2; //*
+const int buttonPin[NButtons] = {2, 3}; //* the number of the pushbutton pins in the desired not order
+int buttonCState[NButtons] = {0};         // stores the button current value
+int buttonPState[NButtons] = {0};        // stores the button previous value
+//byte pin13index = 3; // put the index of the pin 13 in the buttonPin[] if you are using it, if not, comment lines 68-70
 
 /////////////////////////////////////////////
-
-byte midiCh = 1; // *Midi channel to be used 1-16
-byte note = 36; // *Lowest note that will be used
-byte cc = 1; // *Lowest CC that will be used
+// debounce
+unsigned long lastDebounceTime[NButtons] = {0};  // the last time the output pin was toggled
+unsigned long debounceDelay = 5;    //* the debounce time; increase if the output flickers
 
 /////////////////////////////////////////////
+// potentiometers
 
-int TIMEOUT = 300; //Amount of time that the potentiometer will be read after it exceeds the varThreshold
-int varThreshold = 7; // *Threshold for the potentiometer signal variation
+const int NPots = 2; //*
+int potPin[NPots] = {A0, A1}; //* Pin where the potentiometer is
+int potCState[NPots] = {0}; // Current state of the pot
+int potPState[NPots] = {0}; // Previous state of the pot
+int potVar = 0; // Difference between the current and previous state of the pot
+
+int midiCState[NPots] = {0}; // Current state of the midi value
+int midiPState[NPots] = {0}; // Previous state of the midi value
+
+int TIMEOUT = 300; //* Amount of time the potentiometer will be read after it exceeds the varThreshold
+int varThreshold = 10; //* Threshold for the potentiometer signal variation
 boolean potMoving = true; // If the potentiometer is moving
-unsigned long pTime[NPots] = {0}; // Previously stored time
-unsigned long timer[NPots] = {0}; // Stores the time that has elapsed since the timer was reseted
+unsigned long PTime[NPots] = {0}; // Previously stored time
+unsigned long timer[NPots] = {0}; // Stores the time that has elapsed since the timer was reset
 
 /////////////////////////////////////////////
 
-void setup () {
+byte midiCh = 1; //* MIDI channel to be used
+byte note = 36; //* Lowest note to be used
+byte cc = 1; //* Lowest MIDI CC to be used
 
-  //Start midi connection
-  MIDI.begin();
-  //Serial Connection
-  Serial.begin(115200); // Use 115200 for Hairless midi or 31250 for a physical midi port
-  //Initialize Digital Pins as inputs
-  for (int i=0; i<NButtons; i++){
-    pinMode(button[i], INPUT_PULLUP);
+void setup() {
+
+  //Serial.begin(115200); // use if using with ATmega328 (uno, mega, nano...)
+
+  for (int i = 0; i < NButtons; i++) {
+    pinMode(buttonPin[i], INPUT_PULLUP);
   }
-  
-  
-  //Initialize Analog Pins as inputs
-  for (int i=0; i<NPots; i++){
-    pinMode(pot[i], INPUT);
-  }
-  
+  //pinMode(buttonPin[3], INPUT); //pin 13
 
 }
 
-void loop () {
+void loop() {
 
-  //Reads all digital inputs
-  for (int i=0; i<NButtons; i++) {
-    buttonCState[i] = digitalRead(button[i]);
-  }
-    
-  //Reads the state of the buttons and send NoteOns to each one that is active
-  for (int i=0; i<NButtons; i++) {
+  buttons();
+  potentiometers();
 
-    if (buttonCState[i] != buttonPState[i]) {
+}
 
-      if(buttonCState[i] == LOW) {     
-        MIDI.sendNoteOn(note+i, 127, midiCh); // sends NoteOn(note, velocity, midi channel)
-        buttonPState[i] = buttonCState[i];
-      }
-      else {
-        MIDI.sendNoteOn(note+i, 0, midiCh); 
-        buttonPState[i] = buttonCState[i];
-      }
-    }
-    
-  }
+/////////////////////////////////////////////
+// BUTTONS
+void buttons() {
 
-  ////////////////////////////////////////////////////////////////////////////////////////
+  for (int i = 0; i < NButtons; i++) {
 
-  for (int i=0; i<NPots; i++) { // Reads all analog inputs used
-    potCState[i] = analogRead(pot[i]);
-  }
-
-    /* So that only the analog ports are read when they are used, without losing resolution,
-     You must set a threshold (varThreshold), a minimum value that the pots have to have to be moved
-     To start reading. After this, a sort of "door" is created, a door that opens and allows
-    Analogue ports to be read without interruption for a certain time (TIMEOUT). When the timer is less than TIMEOUT
-    Means that the potentiometer has been shaken for a very short time, meaning that it is probably still moving,
-    Then, the door should be kept open; If the timer is longer than TIMEOUT means that it has not been moved for a while,
-    Then, the door should be closed. For this logic to happen, the timer must be reset (lines 99 and 100) each time the analogue port
-    is bigger than the established varThreshold.
+    buttonCState[i] = digitalRead(buttonPin[i]);
+    /*
+        // Comment this if you are not using pin 13...
+        if (i == pin13index) {
+          buttonCState[i] = !buttonCState[i]; //inverts pin 13 because it has a pull down resistor instead of a pull up
+        }
+        // ...until here
     */
-  for (int i=0; i<NPots; i++) {
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) {
 
-    potVar = abs(potCState[i] - potPState[i]); // Computes the analogue port variation
+      if (buttonPState[i] != buttonCState[i]) {
+        lastDebounceTime[i] = millis();
 
-    if (potVar >= varThreshold) {  //Sets the threshold for the variance in the pot state, if it varies more than x it sends the cc message
-      pTime[i] = millis(); // Stores the previous time
+        if (buttonCState[i] == LOW) {
+          // use if using with ATmega328 (uno, mega, nano...)
+          //MIDI.sendNoteOn(note + i, 127, midiCh); // note, velocity, channel
+
+          // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+          noteOn(midiCh, note + i, 127);  // channel, note, velocity
+          MidiUSB.flush();
+
+          //          Serial.print("button on  >> ");
+          //          Serial.println(i);
+        }
+        else {
+          // use if using with ATmega328 (uno, mega, nano...)
+          //MIDI.sendNoteOn(note + i, 0, midiCh); // note, velocity, channel
+
+          // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+          noteOn(midiCh, note + i, 0);  // channel, note, velocity
+          MidiUSB.flush();
+
+          //          Serial.print("button off >> ");
+          //          Serial.println(i);
+        }
+        buttonPState[i] = buttonCState[i];
+      }
     }
-    timer[i] = millis() - pTime[i]; // Resets the timer
+  }
+}
+
+/////////////////////////////////////////////
+// POTENTIOMETERS
+void potentiometers() {
+
+  for (int i = 0; i < NPots; i++) { // Loops through all the potentiometers
+
+    potCState[i] = analogRead(potPin[i]); // Reads the pot and stores it in the potCState variable
+    midiCState[i] = map(potCState[i], 0, 1023, 127, 0); // Maps the reading of the potCState to a value usable in midi
+
+
+    potVar = abs(potCState[i] - potPState[i]); // Calculates the absolute value between the difference between the current and previous state of the pot
+
+    if (potVar > varThreshold) { // Opens the gate if the potentiometer variation is greater than the threshold
+      PTime[i] = millis(); // Stores the previous time
+    }
+
+    timer[i] = millis() - PTime[i]; // Resets the timer 11000 - 11000 = 0ms
+
     if (timer[i] < TIMEOUT) { // If the timer is less than the maximum allowed time it means that the potentiometer is still moving
       potMoving = true;
     }
@@ -118,12 +145,41 @@ void loop () {
       potMoving = false;
     }
 
-    if (potMoving == true) { // If the potentiometer is still moving, it sends the control change
-      MIDI.sendControlChange(cc+i, map(potCState[i], 0, 1023, 0, 127), midiCh); // Sends Control Change (CC number, CC value, midi channel)
-      potPState[i] = potCState[i]; // Stores the current reading of the potentiometer to compare with the next
+    if (potMoving == true) { // If the potentiometer is still moving, send the change control
+      if (midiPState[i] != midiCState[i]) {
+
+        // use if using with ATmega328 (uno, mega, nano...)
+        //MIDI.sendControlChange(cc+i, midiCState[i], midiCh);
+
+        // use if using with ATmega32U4 (micro, pro micro, leonardo...)
+        controlChange(midiCh, cc + i, midiCState[i]); // manda control change (channel, CC, value)
+        MidiUSB.flush();
+
+
+        //Serial.println(midiCState);
+        potPState[i] = potCState[i]; // Stores the current reading of the potentiometer to compare with the next
+        midiPState[i] = midiCState[i];
+      }
     }
   }
-  
+
+}
+
+/////////////////////////////////////////////
+// Arduino (pro)micro midi functions MIDIUSB Library
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
 }
 
 
